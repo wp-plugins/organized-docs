1a95323fad4e73a93b9c888051555cb6f530ea44
isabel104 <isabel104@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-05-20 21:08:38 +0000

    Moved single nav links to template tag. Include missing comments on single. Eliminiated 3 PHP notices. Updated translation file.
    git-svn-id: https://plugins.svn.wordpress.org/organized-docs/trunk@918218 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- organized-docs.php
+++ organized-docs.php
@@ -3,7 +3,7 @@
  * Plugin Name: Organized Docs
  * Plugin URI: http://isabelcastillo.com/docs/category/organized-docs-wordpress-plugin
  * Description: Easily create organized documentation for multiple products, organized by product, and by subsections within each product.
- * Version: 2.0.2
+ * Version: 2.0.3
  * Author: Isabel Castillo
  * Author URI: http://isabelcastillo.com
  * License: GPL2
@@ -296,20 +296,24 @@ class Isa_Organized_Docs{
 		
 		// get term children and sort them by custom sort oder
 		$termchildren =  get_term_children( $top_level_parent_term_id, 'isa_docs_category' );
-		$sorted_termchildren = $this->sort_terms( $termchildren, 'subheading_sort_order' );
-	
-		foreach ( $sorted_termchildren as $sorted_termchild_id => $sorted_termchild_order ) { 
+		if($termchildren) {
+			$sorted_termchildren = $this->sort_terms( $termchildren, 'subheading_sort_order' );
+			if($sorted_termchildren) {
+			
+				foreach ( $sorted_termchildren as $sorted_termchild_id => $sorted_termchild_order ) { 
 
-				$termobject = get_term_by( 'id', $sorted_termchild_id, 'isa_docs_category' );
-				$docs_menu .= '<li class="menu-item';
-		
-				// if current term id matches an id of a child term in menu, then give it active class
-				if ( $termobject->term_id == $curr_term_id_as_int ) {
-					$docs_menu .= ' active-docs-item current_page_item';
+					$termobject = get_term_by( 'id', $sorted_termchild_id, 'isa_docs_category' );
+					$docs_menu .= '<li class="menu-item';
+			
+					// if current term id matches an id of a child term in menu, then give it active class
+					if ( $termobject->term_id == $curr_term_id_as_int ) {
+						$docs_menu .= ' active-docs-item current_page_item';
+					}
+					$docs_menu .= '"><a href="' . get_term_link( $termobject ) . '" title="' . esc_attr( $termobject->name ) . '">' . $termobject->name . '</a></li>';
+			
 				}
-				$docs_menu .= '"><a href="' . get_term_link( $termobject ) . '" title="' . esc_attr( $termobject->name ) . '">' . $termobject->name . '</a></li>';
-		
-		}		
+			}
+		}
 		
 		$docs_menu .= '</ul></div>';
 
@@ -1043,7 +1047,64 @@ class Isa_Organized_Docs{
 		}
 		return $posts;
 	}
+	
+	/**
+	* Displays prev/next nav links for single docs
+	* @since 2.0.3
+	*/
+	
+	public function organized_docs_post_nav() {
+		global $post;
+		$term_list = wp_get_post_terms($post->ID, 'isa_docs_category', array("fields" => "slugs"));
+		// get_posts in same custom taxonomy
 
+		// sort terms by chosen orderby
+		$single_sort_by = get_option('od_single_sort_by');
+		$orderby_order = get_option('od_single_sort_order');
+				
+		if ( 'date' == $single_sort_by ) {
+			$orderby = 'date';
+		} elseif ( 'title - alphabetical' == $single_sort_by ) {
+			$orderby = 'title';
+		} else {
+			$orderby = 'meta_value_num';
+		}
+				
+		$postlist_args = array(
+				'posts_per_page'	=> -1,
+				'post_type'			=> 'isa_docs',
+				'meta_key'			=> '_odocs_meta_sortorder_key',
+				'orderby'			=> $orderby,
+				'order'				=> $orderby_order,
+				'isa_docs_category' => $term_list[0]
+		); 
+		$postlist = get_posts( $postlist_args );
+					
+		// get ids of posts retrieved from get_posts
+		$ids = array();
+		$titles = array();
+		foreach ($postlist as $thepost) : setup_postdata( $thepost );
+			$ids[] = $thepost->ID;
+			$titles[] = $thepost->post_title;
+		endforeach; 
+		wp_reset_postdata();
+		// get and echo previous and next post in the same taxonomy        
+		$thisindex = array_search($post->ID, $ids);
+		$previd = isset($ids[$thisindex-1]) ? $ids[$thisindex-1] : '';
+		$nextid = isset($ids[$thisindex+1]) ? $ids[$thisindex+1] : '';
+		$titleon = get_option( 'od_title_on_nav_links' );
+		$out = '<nav class="navigation post-navigation" role="navigation"><h1 class="screen-reader-text">' . __( 'Post navigation', 'organized-docs' ). '</h1><div class="nav-links">';
+		if ( !empty($previd) ) {
+			$anchor_prev = $titleon ? '&larr; ' . $titles[$thisindex-1] : __( '&larr; Previous', 'organized-docs' );
+			$out .= '<span class="meta-nav"><a rel="prev" href="' . get_permalink($previd) . '">' . $anchor_prev. '</a></span>';
+		}
+		if ( !empty($nextid) ) {
+			$anchor_next = $titleon ? $titles[$thisindex+1] . ' &rarr;' : __( 'Next &rarr;', 'organized-docs' );
+			$out .= '<span class="meta-nav"><a rel="next" href="' . get_permalink($nextid) . '">' . $anchor_next . '</a></span>';
+		}
+		$out .= '</div></nav>';
+		echo $out;
+	}
 	/**
 	 * For backwards compatibility, give all single Docs posts a default sort-order number of 99999
 	 * @since 1.1.8
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=me%40
 Tags: documentation, docs, organize documentation, organized documentation, instruction guides, wiki
 Requires at least: 3.9
 Tested up to: 3.9.1
-Stable Tag: 2.0.2
+Stable Tag: 2.0.3
 License: GNU Version 2
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -46,6 +46,14 @@ This is no longer an issue since version 2.0.
 3. Single docs post with Table of Contents
 
 == Changelog ==
+
+= 2.0.3 =
+* Tweak: moved  prev/next nav links for single docs to a template tag for easier-to-customize template files.
+* Tweak: put back missing comments into single template.
+* Tweak: taller line-height on main docs page.
+* Tweak: eliminated a PHP notice that was triggered in organized_docs_content_nav when sub-categories were not assigned.
+* Tweak: eliminated 2 PHP notices that were triggered in organized_docs_post_nav.
+* Tweak: updated `.pot` translation file.
 
 = 2.0.2 =
 * Fix: improved styling for mobile devices.
